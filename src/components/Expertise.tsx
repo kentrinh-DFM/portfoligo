c9d8de57adc5a164c67b37d2e609af0e8a118138
kenTrinh <ken.trinh@ymail.com>
2025-04-24 00:46:08 +1000

    New Actions

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,44 +1,41 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faBarChart  } from '@fortawesome/free-solid-svg-icons/faBarChart';
+import { faLaptop  } from '@fortawesome/free-solid-svg-icons/faLaptop';
+import { faReact, faDocker, faPython} from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
+    "Tableau",
+    "PowerBI",
+    "Spotfire",
+    "Python",
+    "AWS Cloud",
+    "IBM Cloud Pak for Data",
+    "HTML",
     "CSS3",
     "SASS",
-    "Flask",
-    "Python",
     "SQL",
-    "PostgreSQL",
-    "Postman"
+    "TypeScript",
+    "JavaScript",
+    "Geospatial Visualisation"
 ];
 
 const labelsSecond = [
     "Git",
     "GitHub Actions",
-    "Docker",
     "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
+    "MS Power Automate",
     "Pandas",
     "Selenium",
 ];
 
 const labelsThird = [
     "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "DeepSeek",
+    "Machine Learning"
 ];
 
 function Expertise() {
@@ -47,22 +44,12 @@ function Expertise() {
         <div className="skills-container">
             <h1>Expertise</h1>
             <div className="skills-grid">
-                <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsFirst.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
+               
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <h3>Automation</h3>
+                    <p>I've improved improve businesses processes by automating repetitive tasks and saving many man-hours.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -70,11 +57,21 @@ function Expertise() {
                         ))}
                     </div>
                 </div>
-
+                 <div className="skill">
+                    <FontAwesomeIcon icon={faBarChart } size="3x"/>
+                    <h3>Decision Making Dashboards</h3>
+                    <p>I've developed comprehensive dashboards and reports that provided actionable insights, enabling key stakeholders to track KPIs and optimize performance either by default or customised visualisation.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsFirst.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <FontAwesomeIcon icon={faLaptop} size="3x"/>
+                    <h3>GenAI & Machine Learning</h3>
+                    <p>I've stayed relevant in the market by learning new AI trend and apply to relevant projects where possible.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,50 +16,50 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="July 2019 - Present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Data Integration and Reporting Specialist</h3>
+            <h4 className="vertical-timeline-element-subtitle">Compass Group - Brisbane </h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              Integration of multiple systems to improve business and reporting process.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="Feb 2019 - Jul 2019"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Data Visualisation Specialist - Contract</h3>
+            <h4 className="vertical-timeline-element-subtitle">BHP - Brisbane</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              IoT reporting of multiple mobile plant including rail and transit.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="Aug 2018 - Feb 2019"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Data Visualisation Specialist - Contract</h3>
+            <h4 className="vertical-timeline-element-subtitle">Anglo American - Brisbane </h4>
             <p>
-              Full-stack Development, API Development, User Experience
+              Custom PowerBI visualisation for monitoring coal mining assets.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
+            date="2016 - 2018"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <h3 className="vertical-timeline-element-title">Solutions Data Analyst</h3>
+            <h4 className="vertical-timeline-element-subtitle">GraphenAI - Singapore</h4>
             <p>
-              Automation, Data Governance, Statistical Analysis
+              Built dashboards and generated web solutions.
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
